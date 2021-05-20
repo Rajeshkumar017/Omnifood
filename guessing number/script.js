bd814b98877ff44cd79263be117cce7f97a9d0d3
Rajeshkumar017 <rr931816@student.nitw.ac.in>
2021-05-20 16:26:54 +0530

    second commit

---
+++ guessing number/script.js
@@ -0,0 +1,52 @@
+'use strict';
+let num = Math.trunc(Math.random() * 20) + 1;
+console.log(num);
+let score = Number(document.querySelector('.score').textContent);
+let highscore = 0;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Please enter a number';
+  } else if (guess < num) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'too low!! try again';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      score = 0;
+      document.querySelector('.score').textContent = score;
+    }
+  } else if (guess > num) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'too high!! try again';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      score = 0;
+      document.querySelector('.score').textContent = score;
+    }
+  } else {
+    document.querySelector('.message').textContent =
+      'Yes!!! you guessed it correct';
+    if (highscore < score) {
+      highscore = score;
+    }
+    document.querySelector('.highscore').textContent = highscore;
+    document.querySelector('.number').textContent = guess;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  }
+});
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  num = Math.trunc(Math.random() * 20) + 1;
+  console.log(num);
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
